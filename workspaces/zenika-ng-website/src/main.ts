86dae52909ae7be0a6ab5a6f8a04a7d31f75bec4
Taktak-fedi <taktakfedi97@gmail.com>
2023-12-13 09:38:18 +0000

    app-standalone

--- workspaces/zenika-ng-website/src/main.ts
+++ workspaces/zenika-ng-website/src/main.ts
@@ -1,6 +1,20 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { WELCOME_MSG } from './app/app.token';
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule),
+        {
+            provide: WELCOME_MSG,
+            useValue: 'Bienvenue sur Zenika Ecommerce',
+        },
+        provideHttpClient(withInterceptorsFromDi()),
+    ]
+})
   .catch((err) => console.error(err));
